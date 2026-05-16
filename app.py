a20dd4e3fbd78f26cc6e6f09ff044bc22b2fad33
sneha282006 <sneha.nmurthysrs@gmail.com>
2026-05-15 14:30:21 -1200

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ import pandas as pd
 # Read CSV file
 df = pd.read_csv("population.csv")
 
-# Show column names
-st.write("Columns in CSV:", df.columns)
-
 # Dashboard title
 st.title("Population Dashboard")
 
 # Show table
 st.write(df)
 
-# Use correct column name
-gender_count = df["Gender"].value_counts()
+# Show column names
+st.write(df.columns)
+
+# Use first text column automatically
+gender_count = df.iloc[:, 1].value_counts()
 
 # Show gender distribution
 st.subheader("Gender Distribution")
@@ -25,4 +25,4 @@ st.bar_chart(gender_count)
 
 # Age chart
 st.subheader("Age Distribution")
-st.bar_chart(df["age"])
+st.bar_chart(df.iloc[:, 2])
