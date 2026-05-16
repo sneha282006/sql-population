010120a716989f0d559e615496e5ad2fca67d080
sneha282006 <sneha.nmurthysrs@gmail.com>
2026-05-15 14:02:00 -1200

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.read_csv("population.csv")
+
+st.title("Population Dashboard")
+
+st.write(df)
+
+gender_count = df["gender"].value_counts()
+
+st.subheader("Gender Distribution")
+st.write(gender_count)
+
+st.bar_chart(gender_count)
+
+st.subheader("Age Distribution")
+st.bar_chart(df["age"])
